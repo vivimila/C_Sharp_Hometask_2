016a1e104e13badec46f411d706fe4cffed86176
vivimila <violamilashevskaya@yandex.by>
2022-08-11 11:32:09 +0300

    Task 10 is solved

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int number = new Random().Next(100, 1000); // все трехзначные числа
+int Vivi (int num) // переменную можно называть как угодно?
+{
+    int firstNumber = num / 10; //находим первые  2 цифры, делением на целое
+    int nov = firstNumber % 10;
+    return nov;
+}
+int result = Vivi(number);
+Console.WriteLine($"Из терхзначного {number} -> вторая цифра {result}");
